abb7226d26da15706a204c17c10a193073a1ff78
Matthew Emery <matthew.emery44@gmail.com>
2017-10-15 14:59:11 -0700

    Added clean csv to results

--- src/first_pass.py
+++ src/first_pass.py
@@ -1,16 +1,19 @@
 import pandas as pd
 
-c_elegans_df = pd.read_csv("../data/C_elegans_CRISPR_sgRNA_Database_Responses.csv")
 
-c_elegans_df.columns = ["Timestamp", "sgRNA", "GeneTarget", "GuideSoftware", "Success", "WormsInjected",
-    "SuccessfulInjections", "InjectorExperienceLevel", "DNA", "Promoter", "Cas9Form", "ScreeningMethod", 
-    "RepairMechanism", "Comments"]
-c_elegans_df["WormsInjected"] = c_elegans_df["WormsInjected"].str.extract(r"^(\d+)").astype(int)
+if __name__ == "__main__":
+    c_elegans_df = pd.read_csv("../data/C_elegans_CRISPR_sgRNA_Database_Responses.csv")
     
-c_elegans_df["SuccessRate"] = c_elegans_df["SuccessfulInjections"] / c_elegans_df["WormsInjected"]
-c_elegans_df["SuccessRate"] = c_elegans_df["SuccessRate"].fillna(0)
-
-c_elegans_df["sgRNAlength"] = c_elegans_df["sgRNA"].apply(len)
-c_elegans_df["sgRNA"] = c_elegans_df["sgRNA"].str.split(", ").apply(lambda x: x[0]).str.upper()
-
-c_elegans_df["GuideSoftware"] = ['Yes' if cell not in ("Yes", "No") else cell for cell in c_elegans_df["GuideSoftware"]]
+    c_elegans_df.columns = ["Timestamp", "sgRNA", "GeneTarget", "GuideSoftware", "Success", "WormsInjected",
+        "SuccessfulInjections", "InjectorExperienceLevel", "DNA", "Promoter", "Cas9Form", "ScreeningMethod", 
+        "RepairMechanism", "Comments"]
+    c_elegans_df["WormsInjected"] = c_elegans_df["WormsInjected"].str.extract(r"^(\d+)").astype(int)
+        
+    c_elegans_df["SuccessRate"] = c_elegans_df["SuccessfulInjections"] / c_elegans_df["WormsInjected"]
+    c_elegans_df["SuccessRate"] = c_elegans_df["SuccessRate"].fillna(0)
+    
+    c_elegans_df["sgRNAlength"] = c_elegans_df["sgRNA"].apply(len)
+    c_elegans_df["sgRNA"] = c_elegans_df["sgRNA"].str.split(", ").apply(lambda x: x[0]).str.upper()
+    
+    c_elegans_df["GuideSoftware"] = ['Yes' if cell not in ("Yes", "No") else cell 
+        for cell in c_elegans_df["GuideSoftware"]]
